88b15dc3ca3840fd6c9dcb90154f47cb1c20b72a
dhanushappireddy <appireddydhanush5@gmail.com>
2025-04-15 00:42:46 +0530

    Hello

--- sample.py
+++ sample.py
@@ -13,7 +13,7 @@ def multiply(a, b):
     return a * b
 
 def divide(a, b):
-    priint("Divide");
+    priint("Divide")
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
