cd1b913ef28555bf53b899986f300ded69f605f2
dhanushappireddy <appireddydhanush5@gmail.com>
2025-04-15 00:23:43 +0530

    Hello

--- sample.py
+++ sample.py
@@ -1,15 +1,19 @@
 # calculator.py
 
 def add(a, b):  # Added a print statement for testing
+    priint("Add");
     return a + b
 
 def subtract(a, b):
+    priint("Subtract")
     return a - b
 
 def multiply(a, b):
+    priint("Multiply");
     return a * b
 
 def divide(a, b):
+    priint("Divide");
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
